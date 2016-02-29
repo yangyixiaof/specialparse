a54186e6090fcbd999f0bbc09d1f97e132d0ab7c
Yixiao Yang <yangyixiaofirst@163.com>
2016-02-29 16:52:47 +0800

    small change.

--- specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
+++ specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
@@ -8,23 +8,16 @@ import SJ8Parse.Java8BaseVisitor;
 import SJ8Parse.Java8Lexer;
 import SJ8Parse.Java8Parser;
 
-/**
- * Hello world!
- *
- */
 public class ParseRoot 
 {
-	public static void ParseOneSentence(String onesentence, Java8BaseVisitor<Integer> evalVisitor) throws Exception {
+	public static String ParseOneSentence(String onesentence, Java8BaseVisitor<Integer> evalVisitor) throws Exception {
 		ANTLRInputStream input = new ANTLRInputStream(onesentence);
 		Java8Lexer lexer = new Java8Lexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		Java8Parser parser = new Java8Parser(tokens);
 		parser.setBuildParseTree(true); // tell ANTLR to build a parse tree
 		ParseTree tree = parser.statement(); // parse
-		
-		//testing
-		System.out.println(tree.toStringTree(parser));
-		
 		evalVisitor.visit(tree);
+		return tree.toStringTree(parser);
     }
 }

5425c76c81f9a2cfb07d95e93edbe1974aae911f
Yixiao Yang <yangyixiaofirst@163.com>
2016-02-29 16:50:44 +0800

    small change.

--- specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
+++ specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
@@ -1,13 +1,10 @@
 package cn.yyx.parse.specialparse;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import AeroSpikeHandle.AeroSpikeKeyJava8Visitor;
+import SJ8Parse.Java8BaseVisitor;
 import SJ8Parse.Java8Lexer;
 import SJ8Parse.Java8Parser;
 
@@ -17,26 +14,17 @@ import SJ8Parse.Java8Parser;
  */
 public class ParseRoot 
 {
-	public static void main(String[] args) throws Exception {
-		String inputFile = null;
-		if (args.length > 0)
-			inputFile = args[0];
-		InputStream is = System.in;
-		if (inputFile != null) {
-			is = new FileInputStream(inputFile);
-		}
-		ANTLRInputStream input = new ANTLRInputStream(is);
+	public static void ParseOneSentence(String onesentence, Java8BaseVisitor<Integer> evalVisitor) throws Exception {
+		ANTLRInputStream input = new ANTLRInputStream(onesentence);
 		Java8Lexer lexer = new Java8Lexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		Java8Parser parser = new Java8Parser(tokens);
 		parser.setBuildParseTree(true); // tell ANTLR to build a parse tree
 		ParseTree tree = parser.statement(); // parse
-		// show tree in text form
+		
+		//testing
 		System.out.println(tree.toStringTree(parser));
-
-		AeroSpikeKeyJava8Visitor evalVisitor = new AeroSpikeKeyJava8Visitor();
-		// int result =
+		
 		evalVisitor.visit(tree);
-		// System.out.println("visitor result = "+result);
     }
 }

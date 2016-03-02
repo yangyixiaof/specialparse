cad0d3ad2f4cba52ecf822f747091a05155af8ac
Yixiao Yang <yangyixiaofirst@163.com>
2016-03-02 14:13:02 +0800

    small change.

--- specialparse/src/main/java/cn/yyx/parse/specialparse/TODOList.java
+++ specialparse/src/main/java/cn/yyx/parse/specialparse/TODOList.java
@@ -5,18 +5,18 @@ import SJ8Parse.Java8Listener;
 
 public class TODOList {
 	
-	// TODO Cast Expression (type & type & type) have not been considered.
-	// TODO to ensure that if no ref, just put the raw string.
-	// TODO What is the use of HackedNoType = <$NT$>?
-	// TODO ArrayInitial = "@ARI"; ArrayCreation = "@ARC"; and Class new instance should not be replaced @... this must be improved in the future.
-	// TODO parameter has dims doesn't take into consideration such as int sd[][]. public void test(String a, int sd[][]){}
-	// TODO method declare has dims at last doesn't take into consideration such as : public int test(String a, int sd[][])[] {return null;} which equals to public int[] test(String a, int sd[][]).
-	// TODO what is receiverParameter???
-	// TODO method declaration must be post-fixed with '@MD'
-	// TODO class declaration must be post-fixed with '@CD'
-	// TODO typeArguments not handled, such as '<? extends Integer>'.
-	// TODO catch exceptionTypeList such as catch (AException || BException || CException);. Plus catch#...#... change to catch(...||...)
-	// TODO extends, implements doesn't take into consideration.
+	// solved. Cast Expression (type & type & type) have not been considered.
+	// solved. to ensure that if no ref, just put the raw string.
+	// solved. What is the use of HackedNoType = <$NT$>?
+	// solved. ArrayInitial = "@ARI"; ArrayCreation = "@ARC"; and Class new instance should not be replaced @... this must be improved in the future.
+	// solved. parameter has dims doesn't take into consideration such as int sd[][]. public void test(String a, int sd[][]){}
+	// solved. method declare has dims at last doesn't take into consideration such as : public int test(String a, int sd[][])[] {return null;} which equals to public int[] test(String a, int sd[][]).
+	// solved. what is receiverParameter???
+	// solved. method declaration must be post-fixed with '@MD'
+	// solved. class declaration must be post-fixed with '@CD'
+	// solved. typeArguments not handled, such as '<? extends Integer>'.
+	// solved. catch exceptionTypeList such as catch (AException || BException || CException);. Plus catch#...#... change to catch(...||...)
+	// solved. extends, implements doesn't take into consideration.
 	
 	public static void test()
 	{

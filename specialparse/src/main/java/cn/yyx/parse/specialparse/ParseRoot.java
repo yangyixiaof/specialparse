687c26608e1542b103647cc953f1ae5ef4052657
yyx <yyx@ubuntu>
2016-04-23 04:23:44 -0700

    small change.

--- specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
+++ specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
@@ -10,7 +10,7 @@ import SJ8Parse.Java8Parser;
 
 public class ParseRoot 
 {
-	public static String ParseOneSentence(String onesentence, Java8BaseVisitor<Integer> evalVisitor, boolean returnTreeContent) throws Exception {
+	public static String ParseOneSentence(String onesentence, Java8BaseVisitor<Integer> evalVisitor, boolean returnTreeContent) throws Exception, Error {
 		ANTLRInputStream input = new ANTLRInputStream(onesentence);
 		Java8Lexer lexer = new Java8Lexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);

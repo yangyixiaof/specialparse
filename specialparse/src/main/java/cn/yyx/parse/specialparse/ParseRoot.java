c19774f3dba60f227b8adbcded7925247a0168a1
yyx <yyx@ubuntu>
2016-04-25 21:28:01 -0700

    small change.

--- specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
+++ specialparse/src/main/java/cn/yyx/parse/specialparse/ParseRoot.java
@@ -52,6 +52,7 @@ public class ParseRoot
 			ParseOneSentence("MI@super(@C0?0)", null, true);
 			ParseOneSentence("MI@this(this,'$')", null, true);
 			ParseOneSentence("MR@new::AboutController", null, true);
+			// TODO this two needs to be checked to see if program processor is wrong.
 			ParseOneSentence("N@@C0?0", null, true);
 			ParseOneSentence("N@@F0?0", null, true);
 		} catch (Exception e) {
